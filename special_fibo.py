06aad15f5dc26b079d9e70351a82c58f7b41f3ee
PravalikaThummanapelly <pravalikathummanapelly@gmail.com>
2025-09-22 22:01:53 +0530

    Add files via upload

---
+++ special_fibo.py
@@ -0,0 +1,19 @@
+'''Problem Statement: 
+Alex is exploring a series and she came across a special series,
+ in which f(N)=f(N-1)*f(N-1)+(N-2)*(N-2) mod 47 
+ where f(0) = 1. f(1)=1 
+ Your task is to help Alex find and return an integer value, 
+ representing the Nth  number in this special series. 
+   Sample Input: 4 
+   Sample Output: 29'''
+
+def spcl_fibo(n):
+    if n==0 or n==1:
+        return 1
+    f0,f1=1,1
+    for i in range(2,n+1):
+        fn=(f1*f1+f0*f0)%47
+        f0 , f1=f1,fn
+    return fn 
+n=int(input())
+print(spcl_fibo(n))
